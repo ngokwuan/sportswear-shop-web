ef2b13a872169b245a226e6e7045564f283f4f11
ngokwuan <nnquan271204@gmail.com>
2025-08-12 20:35:59 +0700

    add header, footer separate router

--- frontend/src/layouts/DefaultLayout/index.jsx
+++ frontend/src/layouts/DefaultLayout/index.jsx
@@ -1,5 +1,24 @@
-function DefaultLayout() {
-  return <div>Default Layout</div>;
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
+function DefaultLayout({ children }) {
+  return (
+    <div className={cx('wrapper')}>
+      <Header />
+      <div className={cx('container')}>
+        {/*de load content vao trong children */}
+        <div className={cx('content')}>{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
+DefaultLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default DefaultLayout;
